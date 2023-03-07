031f35547e9fc5d123dfa6700925bd1aae8c619f
Anton Yosifov <yosifov.anton@gmail.com>
2023-03-08 00:50:05 +0200

    Create navTabs componet and pages components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavTabs from './components/NavTabs';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Work from './components/pages/Work';
+import Skills from './components/pages/Skills';
+import Contact from './components/pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <NavTabs />
+        {/* Wrap Route elements in a Routes component */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="work" element={<Work />} />
+          <Route path="skills" element={<Skills />} />
+          <Route path="contact/*" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
