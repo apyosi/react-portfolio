8d651015f7542a7d8d85081a16acbcfe632651fe
Anton Yosifov <yosifov.anton@gmail.com>
2023-03-12 08:40:11 +0200

    Skills component - update

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -2,14 +2,73 @@ import React from "react";
 
 function Skills() {
   return (
-    <section className="skills">
-      <h1>Skills</h1>
-      <p>
-        Lorem ipsum dolor sit amet, est ut enim consequat. Nostrum fastidii partiendo sed ne, no
-        mutat ludus aperiri mea, per in choro dolorem electram. Invidunt reprimique assueverit quo
-        ne, eruditi graecis pro ut. Usu ut diceret scaevola evertitur, appareat voluptatibus ad vel.
-      </p>
-    </section>
+    <>
+      <section id="skills">
+        <h1 className="text-center">Skills</h1>
+        <hr />
+        <div className="container text-center">
+          <div className="list-unstyled row justify-content-evenly">
+            <div className="col-4">
+              <i
+                className="fa-brands fa-html5 fa-7x text-danger"
+                aria-hidden="true"
+              ></i>
+              <p>HTML</p>
+            </div>
+            <div className="col-4">
+              <i
+                className="fa-brands fa-css3-alt fa-7x text-primary"
+                aria-hidden="true"
+              ></i>
+              <p>CSS</p>
+            </div>
+            <div className="col-4">
+              <i
+                className="fa-brands fa-bootstrap fa-7x text-info"
+                aria-hidden="true"
+              ></i>
+              <p>Bootstrap</p>
+            </div>
+            <div className="col-4">
+              <i
+                className="fa-brands fa-js fa-7x text-warning"
+                aria-hidden="true"
+              ></i>
+              <p>Javascript</p>
+            </div>
+            <div className="col-4">
+              <i
+                className="fa-brands fa-node fa-7x text-success"
+                aria-hidden="true"
+              ></i>
+              <p>Node.js</p>
+            </div>
+            <div className="col-4">
+              <i
+                className="fa-brands fa-react fa-7x text-info"
+                aria-hidden="true"
+              ></i>
+              <p>React</p>
+            </div>
+            <div className="col-4">
+              <i
+                className="fa-brands fa-square-git fa-7x"
+                aria-hidden="true"
+              ></i>
+              <p>Git</p>
+            </div>
+            <div className="col-4">
+              <i className="fa-brands fa-github fa-7x" aria-hidden="true"></i>
+              <p>GitHub</p>
+            </div>
+            <div className="col-4">
+              <i className="fa-brands fa-gitlab fa-7x" aria-hidden="true"></i>
+              <p>GitLab</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
 
