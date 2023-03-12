c41d7f11613de143fcb139a4bf7bbf46caa172fb
Anton Yosifov <yosifov.anton@gmail.com>
2023-03-13 01:58:04 +0200

    change class name

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -13,7 +13,7 @@ const NotFound = () => {
 
   return (
     <>
-      <section className="error"></section>
+      <section id="notFound"></section>
       <h1>404</h1>
       <h2>Page Not Found</h2>
       {/* <Link to="/" className="btn">
