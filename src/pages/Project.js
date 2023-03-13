34a6ff9b2f193363622d9939e5652e1ee546de3e
Anton Yosifov <yosifov.anton@gmail.com>
2023-03-14 00:40:06 +0200

    Add message for not found project

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import projects from "../data.json";
 
 const Project = () => {
@@ -12,11 +12,20 @@ const Project = () => {
           <>
             <h1 >{project.id}</h1>
             <p>{project.description}</p>
-          <div><a href={project.html_url} target="_blank"><button className="btn bg-dark text-white m-2">GitHub Repository</button></a>
-            <a href={project.deployed_url} target="_blank"><button className="btn bg-dark text-white">Deployed link</button></a></div>
-            <img className="border border-dark m-1" src={project.image_url} alt={projects.id} />
+          <div><a href={project.html_url} target="_blank" rel="noreferrer"><button className="btn bg-dark text-white m-2">GitHub Repository</button></a>
+            <a href={project.deployed_url} target="_blank" rel="noreferrer"><button className="btn bg-dark text-white">Deployed link</button></a></div>
+            <img className="border border-dark m-1" src={project.image_url} alt={projects.id} width="700"   />
           </>
         }
+        {!project &&
+                    <>
+                        <h2>Project Not Found</h2>
+                        <p>Well, that's disappointing.</p>
+                        <p>
+                            <Link to='/projects'>Visit project section</Link>
+                        </p>
+                    </>
+                }
       </article>
       
     </main>
