c8ed0cee7c8f591f5437dfba455d8bf05280c943
Anton Yosifov <yosifov.anton@gmail.com>
2023-03-13 02:00:38 +0200

    refactor app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
-import About from "./pages/About";
-import Work from "./pages/Projects";
+import Projects from "./pages/Projects";
+import Project from "./pages/Project";
 import Skills from "./pages/Skills";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
+import data from "./data.json"
 
 function App() {
+  // const [projects, setProjects] = useState([data]);
+  // const projectss = JSON.parse(data);
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/work" element={<Work />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="projects">
+          <Route index element={<Projects />} />
+          <Route path=":id" element={<Project />} />
+        </Route>
+        <Route path="skills" element={<Skills />} />
+        <Route path="contact" element={<Contact />} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
