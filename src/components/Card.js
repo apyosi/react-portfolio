e30a559a8a6ed51b721625d08f55d9d24917c8a7
Anton Yosifov <yosifov.anton@gmail.com>
2023-03-14 00:39:18 +0200

    refactor card componet

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Card = ({item}) => {
+const Card = ({ item }) => {
   return (
-    <article className="card col-md-4 col-sm-6 border-0">
-      <Link to={`${item.id}`}>
-        <img src={item.image_url} className="card-img-top" alt={item.id} width="100%"/>
-        <div className="card-body">
-          <h5 className="card-title">{item.id}</h5>
-          <p className="card-text">
-          {item.description}
-          </p>
-        </div>
-      </Link>
+    <article className="card col-md-4 col-sm-6  bg-black border-0" >
+      <div className="text-center w-100 p-2 border border-0 border-light ">
+        <Link to={`${item.id}`}>
+          <h3 className="card-title">{item.id}</h3>
+        </Link>
+        <p className="card-text">{item.description}</p>
+        <Link to={`${item.id}`}>
+          <img src={item.image_url} className="img-fluid img-thumbnail border border-1 border-white" width="300" height="300" alt={item.id} />
+        </Link>
+      </div>
     </article>
   );
 };
