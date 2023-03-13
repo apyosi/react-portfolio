6f59cf92190fab42a6c4fd528d4977839407ae9c
Anton Yosifov <yosifov.anton@gmail.com>
2023-03-14 00:37:21 +0200

    Update the contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,24 +1,55 @@
-import React from 'react';
-
+import React from "react";
 
 function Contact(props) {
   return (
-    <section id="contact">
-      <h1>Contact</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
-    </section>
+    <main id="contact">
+      <h1 className="text-center">Contact</h1>
+      <img
+        className="w-100 mb-3"
+        src="https://user-images.githubusercontent.com/73097560/115834477-dbab4500-a447-11eb-908a-139a6edaec5c.gif"
+        alt="lighting-line"
+      />
+      <form className="w-75 m-auto">
+        <div className="mb-3">
+          <label for="exampleInputName" className="form-label">
+            Name
+          </label>
+          <input
+            type="text"
+            className="form-control bg-warning"
+            id="exampleInputName"
+            aria-describedby="nameHelp"
+          />
+        </div>
+        <div className="mb-3">
+          <label for="exampleInputEmail1" className="form-label">
+            Email
+          </label>
+          <input
+            type="email"
+            className="form-control bg-warning"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div className="mb-3">
+          <label for="exampleFormControlTextarea1" className="form-label">
+            Message
+          </label>
+          <textarea
+            className="form-control bg-warning"
+            id="exampleFormControlTextarea1"
+            rows="3"
+          ></textarea>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </main>
   );
 }
 
